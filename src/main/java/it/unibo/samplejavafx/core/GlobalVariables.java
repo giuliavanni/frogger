fedad005d321bd9a029b7ef30bea3806bff77a56
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-02-06 16:16:49 +0100

    ADD file for global variables

---
+++ src/main/java/it/unibo/samplejavafx/core/GlobalVariables.java
@@ -0,0 +1,13 @@
+package it.unibo.samplejavafx.core;
+
+public final class GlobalVariables {
+    // Prevent instantiation
+    private GlobalVariables() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    // Define global variables
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final int JUMP_SIZE = 46;
+}
